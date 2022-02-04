7fba63bcc3178e7a6361336cc9aca036157cd005
JasonW25 <95110713+JasonW25@users.noreply.github.com>
2022-02-04 17:37:14 -0600

    Update notification_manager.py

--- 100daypython/Flight-deals/notification_manager.py
+++ 100daypython/Flight-deals/notification_manager.py
@@ -5,18 +5,18 @@ import smtplib
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self) -> None:
-        self.auth_token = "7289826a2db4e05489eb932cf398d75a"
-        self.account_sid = "AC5f0fb9800e496e154c54467a73f53c64"
-        self.sheety_endpoint = "https://api.sheety.co/178b21c86acd124f8b551671aa235d53/flightUsers/sheet1"
-        self.token = "fn23frf823f8jd9kck3hfcevg7383df"
+        self.auth_token = ""
+        self.account_sid = ""
+        self.sheety_endpoint = ""
+        self.token = ""
 
     def send_message(self, text):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages \
                     .create(
                         body=text,
-                        from_='+19792726617',
-                        to='+12144177306'
+                        from_='',
+                        to=''
                     )
         print(message.status)
 
@@ -33,7 +33,7 @@ class NotificationManager:
 
             connection = smtplib.SMTP("smtp.gmail.com")
             connection.starttls()
-            connection.login(user=my_email, password="Testpassword250")
+            connection.login(user=my_email, password=" ")
             connection.sendmail(from_addr=my_email, to_addrs=bday_email_address, msg="Subject:Flight Deals\n\n" + email_text)
             connection.close()
 
